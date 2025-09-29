8d72797f1315acc497a58c8363fd35eb603df6dc
duongthuan0405 <duongthuan0405@gmail.com>
2025-09-29 20:44:30 +0700

    Return mono selected

---
+++ Assets/ProjectAssets/Scripts/CellSelector.cs
@@ -0,0 +1,61 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CellSelector : MonoBehaviour
+{
+    private GameObject selectedObject;
+    private Camera cam;
+
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    void Update()
+    {
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            Ray ray = cam.ScreenPointToRay(Mouse.current.position.ReadValue());
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            {
+                GameObject clickedObj = hit.collider.gameObject;
+                OnObjectSelected(clickedObj);
+            }
+            else
+            {
+                OnObjectDeselected();
+            }
+        }
+    }
+
+    private void OnObjectSelected(GameObject obj)
+    {
+        OnObjectDeselected();
+        selectedObject = obj;
+        Renderer renderer = obj.GetComponent<Renderer>();
+        if (renderer != null)
+        {
+            renderer.material.EnableKeyword("_EMISSION");
+        }    
+    
+
+    }
+    
+    private void OnObjectDeselected()
+    {
+        if(selectedObject == null)
+        {
+            return;
+        }
+        Renderer renderer = selectedObject.GetComponent<Renderer>();
+        if (renderer != null)
+        {
+            renderer.material.DisableKeyword("_EMISSION");
+        }
+        selectedObject = null;
+    }
+
+}
